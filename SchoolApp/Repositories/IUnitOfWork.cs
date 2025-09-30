01f07aed21e2ea555a5c0e847287af3f34036913
a8anassis <a8anassis@gmail.com>
2025-09-30 20:51:56 +0300

    feat: add unit of work

---
+++ SchoolApp/Repositories/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿namespace SchoolApp.Repositories
+{
+    public interface IUnitOfWork
+    {
+        UserRepository UserRepository { get; }
+        // StudentRepository StudentRepository { get; }
+        TeacherRepository TeacherRepository { get; }
+        //CourseRepository CourseRepository { get; }
+
+        Task<bool> SaveAsync();
+    }
+}
