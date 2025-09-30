e32f4a10898bd1f8a85b04fbc1ff9782bac72d2e
a8anassis <a8anassis@gmail.com>
2025-09-30 20:27:35 +0300

    feat: add teacher repository

--- SchoolApp/Repositories/ITeacherRepository.cs
+++ SchoolApp/Repositories/ITeacherRepository.cs
@@ -8,8 +8,9 @@ namespace SchoolApp.Repositories
     {
         Task<List<Course>> GetTeacherCoursesAsync(int teacherId);
         Task<Teacher?> GetByPhoneNumberAsync(string phoneNumber);
-        Task<List<User>> GetAllUsersTeachersAsync();
         Task<User?> GetUserTeacherByUsernameAsync(string username);
+        
+        Task<List<User>> GetAllUsersTeachersAsync();
         Task<PaginatedResult<User>> GetPaginatedUsersTeachersAsync(int pageNumber, int pageSize);
         Task<PaginatedResult<User>> GetPaginatedUsersTeachersFilteredAsync(int pageNumber, int pageSize, 
             List<Expression<Func<User, bool>>> predicates);
--- SchoolApp/Repositories/TeacherRepository.cs
+++ SchoolApp/Repositories/TeacherRepository.cs
@@ -12,13 +12,6 @@ namespace SchoolApp.Repositories
         {
         }
 
-        public async Task<Teacher?> GetByPhoneNumberAsync(string phoneNumber)
-        {
-            return await context.Teachers
-                .Where(t => t.PhoneNumber == phoneNumber)
-                .FirstOrDefaultAsync();
-        }
-
         public async Task<List<Course>> GetTeacherCoursesAsync(int teacherId)
         {
             List<Course> courses;
@@ -32,6 +25,25 @@ namespace SchoolApp.Repositories
 
         }
 
+        public async Task<Teacher?> GetByPhoneNumberAsync(string phoneNumber)
+        {
+            return await context.Teachers
+                .Where(t => t.PhoneNumber == phoneNumber)
+                .FirstOrDefaultAsync(); // fetched many and select the first or default
+        }
+
+
+        public async Task<User?> GetUserTeacherByUsernameAsync(string username)
+        {
+            var userTeacher = await context.Users
+                .Where(u => u.Username == username && u.UserRole == UserRole.Teacher)
+                .Include(u => u.Teacher) // Εager loading της σχετικής οντότητας Teacher
+                .SingleOrDefaultAsync();    // fetces 0 or 1 results, otherwise throws an exception
+
+            return userTeacher;
+        }
+        
+
         public async Task<List<User>> GetAllUsersTeachersAsync()
         {
             var usersWithRoleTeacher = await context.Users
@@ -42,24 +54,55 @@ namespace SchoolApp.Repositories
             return usersWithRoleTeacher;
         }
 
-        public Task<PaginatedResult<User>> GetPaginatedUsersTeachersAsync(int pageNumber, int pageSize)
+        public async Task<PaginatedResult<User>> GetPaginatedUsersTeachersAsync(int pageNumber, int pageSize)
         {
-            throw new NotImplementedException();
+            int skip = (pageNumber - 1) * pageSize;
+
+            var usersWithRoleTeacher = await context.Users
+                .Where(u => u.UserRole == UserRole.Teacher)
+                .Include(u => u.Teacher) // Εager loading της σχετικής οντότητας Teacher   
+                .Skip(skip)
+                .Take(pageSize)
+                .ToListAsync();
+
+            int totalRecords = await context.Users
+                .Where(u => u.UserRole == UserRole.Teacher)
+                .CountAsync();
+
+            return new PaginatedResult<User>(usersWithRoleTeacher, totalRecords, pageNumber, pageSize);
         }
 
         
 
-        public  Task<PaginatedResult<User>> GetPaginatedUsersTeachersFilteredAsync(int pageNumber, int pageSize, 
+        public async Task<PaginatedResult<User>> GetPaginatedUsersTeachersFilteredAsync(int pageNumber, int pageSize, 
             List<Expression<Func<User, bool>>> predicates)
         {
-            throw new NotImplementedException();
+            IQueryable<User> query = context.Users
+                .Where(u => u.UserRole == UserRole.Teacher)
+                .Include(u => u.Teacher); // Εager loading της σχετικής οντότητας Teacher
+
+            if (predicates != null && predicates.Count > 0) 
+            {
+                foreach (var predicate in predicates)
+                {
+                    query = query.Where(predicate); // εκτελείται, υπονοείται το AND
+                }
+            }
+
+            int totalRecords = await query.CountAsync(); // εκτελείται
+            int skip = (pageNumber - 1) * pageSize;
+            
+            var data = await query
+                .OrderBy(u => u.Id) // πάντα να υπάρχει ένα OrderBy πριν το Skip
+                .Skip(skip)
+                .Take(pageSize)
+                .ToListAsync(); // εκτελείται
+
+            return new PaginatedResult<User>(data, totalRecords, pageNumber, pageSize);
         }
 
        
 
-        public Task<User?> GetUserTeacherByUsernameAsync(string username)
-        {
-            throw new NotImplementedException();
-        }
+        
     }
 }
