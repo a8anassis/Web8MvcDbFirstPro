7ef913024c344e5d751a1a503563e9cb09514641
a8anassis <a8anassis@gmail.com>
2025-09-30 20:52:49 +0300

    feat: add student repository

---
+++ SchoolApp/Repositories/IStudentRepository.cs
@@ -0,0 +1,15 @@
+﻿using SchoolApp.Data;
+using SchoolApp.Models;
+using System.Linq.Expressions;
+
+namespace SchoolApp.Repositories
+{
+    public interface IStudentRepository
+    {
+        Task<List<Course>> GetStudentCoursesAsync(int studentId);
+        Task<Student?> GetByam(string? am);
+        Task<PaginatedResult<Student>> GetPaginatedUsersStudentsAsync(int pageNumber, int pageSize);
+        Task<PaginatedResult<Student>> GetPaginatedUsersStudentsFilteredAsync(int pageNumber, int pageSize,
+            List<Expression<Func<User, bool>>> predicates);
+    }
+}
---
+++ SchoolApp/Repositories/StudentRepository.cs
@@ -0,0 +1,33 @@
+﻿using SchoolApp.Data;
+using SchoolApp.Models;
+using System.Linq.Expressions;
+
+namespace SchoolApp.Repositories
+{
+    public class StudentRepository : BaseRepository<Student>, IStudentRepository
+    {
+        public StudentRepository(Mvc8DbProContext context) : base(context)
+        {
+        }
+
+        public Task<Student?> GetByam(string? am)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PaginatedResult<Student>> GetPaginatedUsersStudentsAsync(int pageNumber, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PaginatedResult<Student>> GetPaginatedUsersStudentsFilteredAsync(int pageNumber, int pageSize, List<Expression<Func<User, bool>>> predicates)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Course>> GetStudentCoursesAsync(int studentId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
