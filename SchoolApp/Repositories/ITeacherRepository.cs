b1c6be2b8e556c342179887bf34bcca15de3beec
a8anassis <a8anassis@gmail.com>
2025-09-30 20:06:04 +0300

    feat: add teacher repository

--- SchoolApp/Repositories/ITeacherRepository.cs
+++ SchoolApp/Repositories/ITeacherRepository.cs
@@ -9,9 +9,9 @@ namespace SchoolApp.Repositories
         Task<List<Course>> GetTeacherCoursesAsync(int teacherId);
         Task<Teacher?> GetByPhoneNumberAsync(string phoneNumber);
         Task<List<User>> GetAllUsersTeachersAsync();
-        Task<List<User>> GetAllUsersTeachersPaginatedAsync(int pageNumber, int pageSize);
         Task<User?> GetUserTeacherByUsernameAsync(string username);
-        Task<PaginatedResult<User>> GetPaginatedUsersTeachersAsync(int pageNumber, int pageSize, 
+        Task<PaginatedResult<User>> GetPaginatedUsersTeachersAsync(int pageNumber, int pageSize);
+        Task<PaginatedResult<User>> GetPaginatedUsersTeachersFilteredAsync(int pageNumber, int pageSize, 
             List<Expression<Func<User, bool>>> predicates);
     }
 }
---
+++ SchoolApp/Repositories/TeacherRepository.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolApp.Core.Enums;
+using SchoolApp.Data;
+using SchoolApp.Models;
+using System.Linq.Expressions;
+
+namespace SchoolApp.Repositories
+{
+    public class TeacherRepository : BaseRepository<Teacher>, ITeacherRepository
+    {
+        public TeacherRepository(Mvc8DbProContext context) : base(context)
+        {
+        }
+
+        public async Task<Teacher?> GetByPhoneNumberAsync(string phoneNumber)
+        {
+            return await context.Teachers
+                .Where(t => t.PhoneNumber == phoneNumber)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<List<Course>> GetTeacherCoursesAsync(int teacherId)
+        {
+            List<Course> courses;
+
+            courses = await context.Teachers
+                .Where(t => t.Id == teacherId)
+                .SelectMany(t => t.Courses)
+                .ToListAsync();
+
+            return courses;
+
+        }
+
+        public async Task<List<User>> GetAllUsersTeachersAsync()
+        {
+            var usersWithRoleTeacher = await context.Users
+                .Where(u => u.UserRole == UserRole.Teacher)
+                .Include(u => u.Teacher) // Εager loading της σχετικής οντότητας Teacher
+                .ToListAsync();
+
+            return usersWithRoleTeacher;
+        }
+
+        public Task<PaginatedResult<User>> GetPaginatedUsersTeachersAsync(int pageNumber, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+
+        public  Task<PaginatedResult<User>> GetPaginatedUsersTeachersFilteredAsync(int pageNumber, int pageSize, 
+            List<Expression<Func<User, bool>>> predicates)
+        {
+            throw new NotImplementedException();
+        }
+
+       
+
+        public Task<User?> GetUserTeacherByUsernameAsync(string username)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
