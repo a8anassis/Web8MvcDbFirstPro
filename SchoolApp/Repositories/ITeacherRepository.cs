011341e45f8cac51e12ccee18ec24d4dfd39156e
a8anassis <a8anassis@gmail.com>
2025-09-30 19:46:34 +0300

    feat: add teacher repository

---
+++ SchoolApp/Repositories/ITeacherRepository.cs
@@ -0,0 +1,17 @@
+﻿using SchoolApp.Data;
+using SchoolApp.Models;
+using System.Linq.Expressions;
+
+namespace SchoolApp.Repositories
+{
+    public interface ITeacherRepository
+    {
+        Task<List<Course>> GetTeacherCoursesAsync(int teacherId);
+        Task<Teacher?> GetByPhoneNumberAsync(string phoneNumber);
+        Task<List<User>> GetAllUsersTeachersAsync();
+        Task<List<User>> GetAllUsersTeachersPaginatedAsync(int pageNumber, int pageSize);
+        Task<User?> GetUserTeacherByUsernameAsync(string username);
+        Task<PaginatedResult<User>> GetPaginatedUsersTeachersAsync(int pageNumber, int pageSize, 
+            List<Expression<Func<User, bool>>> predicates);
+    }
+}
