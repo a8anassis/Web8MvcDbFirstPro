1a6210fdcc33a72756ceb1bf2f009554301086b8
a8anassis <a8anassis@gmail.com>
2025-09-30 20:58:16 +0300

    feat: add course repository

---
+++ SchoolApp/Repositories/CourseRepository.cs
@@ -0,0 +1,21 @@
+﻿using SchoolApp.Data;
+
+namespace SchoolApp.Repositories
+{
+    public class CourseRepository : BaseRepository<Course>, ICourseRepository
+    {
+        public CourseRepository(Mvc8DbProContext context) : base(context)
+        {
+        }
+
+        public Task<List<Student>> GetCourseStudentsAsync(int courseId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Teacher?> GetCourseTeacherAsync(int courseId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
