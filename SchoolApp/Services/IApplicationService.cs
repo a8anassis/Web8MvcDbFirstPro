6a65606fa9f2f87a45061f5ab21173b72702473e
a8anassis <a8anassis@gmail.com>
2025-10-01 20:12:18 +0300

    feat: add user and teacher service

---
+++ SchoolApp/Services/IApplicationService.cs
@@ -0,0 +1,9 @@
+﻿namespace SchoolApp.Services
+{
+    public interface IApplicationService
+    {
+        UserService UserService { get;  }
+        TeacherService TeacherService { get; }
+        // Other services can be added here as needed
+    }
+}
